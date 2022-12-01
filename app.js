23b6d7a41f8bc700fe693fdbe3ed34a4aa04e9a2
cosmos1030 <dyk6208@gmail.com>
2022-12-01 00:05:52 +0000

    save input to dic

---
+++ app.js
@@ -0,0 +1,15 @@
+const submit = document.querySelector('button');
+
+const stock = {};
+
+function onSubmit(event){
+    let stock_name = document.querySelector('#stock_name').value;
+    let stock_quantity = document.querySelector('#stock_quantity').value;
+    stock[stock_name] = stock_quantity;
+    document.querySelector('#output').innerHTML = "";
+    for (var key in stock){
+        document.querySelector('#output').innerHTML += "<li>"+key+": "+stock[key]+"</li>";
+    }
+}
+
+submit.addEventListener('click', onSubmit);
